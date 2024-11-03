cc7befa00822c08b4f2522b06dfeac3cfc0012f8
Tarek Aljamal <tarekaljamal@outlook.com>
2024-11-03 19:19:15 +0200

    A Python function that check the peaks in an array and returns a list of indices of those peaks

---
+++ PeaksinArrays.py
@@ -0,0 +1,10 @@
+
+# A Python function that checks the peaks in an array and returns a list of indices of those peaks
+
+def find_peaks(peaks):
+    stored_peaks = []
+    for i in range(1,len(peaks)-1):
+        if((peaks[i] > peaks[i-1]) & (peaks[i] > peaks[i + 1])):
+            stored_peaks.append(i)
+  
+
